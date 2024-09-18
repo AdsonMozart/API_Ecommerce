a6e165d2c9ff546b6a05e1b5a2108d760d51dce6
AdsonMozart <110190822+AdsonMozart@users.noreply.github.com>
2024-09-18 11:51:48 -0300

    inicio da introdução do banco de dados

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 #Importação
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+#Para criar nossa aplicação
 app = Flask(__name__)
 
-# Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
+#Para definir o caminho do arquivo do nosso banco
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+#Para linkar nossa aplicação com o banco
+db = SQLAlchemy(app)
+
+#Modelagem
+#Produto (id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+#Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
 @app.route('/')
 def hello_world():
     return 'Hello World'
